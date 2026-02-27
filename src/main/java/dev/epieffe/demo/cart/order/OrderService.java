869f46dfb54e1d43074c8f5b789992586fa5b4e4
epieffe.eth <ferrariepifanio@gmail.com>
2026-02-28 00:26:41 +0100

    Add @Transactional annotation to OrderService

--- src/main/java/dev/epieffe/demo/cart/order/OrderService.java
+++ src/main/java/dev/epieffe/demo/cart/order/OrderService.java
@@ -1,12 +1,13 @@
 package dev.epieffe.demo.cart.order;
 
-import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class OrderService {
 
 	private final OrderRepository orderRepository;
